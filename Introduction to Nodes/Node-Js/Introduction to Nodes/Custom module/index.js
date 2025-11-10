6008a1d12d670c6c1fb961e1b227415e480d03b1
Swapnil Patil <patilswapnil222334@gmail.com>
2025-11-10 22:33:01 +0530

    Delete Introduction to Nodes/Node-Js directory

--- Introduction to Nodes/Node-Js/Introduction to Nodes/Custom module/index.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const data = "<html><head><title>Custom Module</title></head></html>";
-const outPath = path.join(__name, 'index.html');
-fs.writeFileSync(outPath, data, 'utf8');
